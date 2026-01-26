8b9df75457d992faad537df7b38b2bf597e5154a
Thembela2 <thembelaveleleni41@gmail.com>
2026-01-26 23:58:11 +0000

    Added Navbar component

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
 import Skills from "./components/Skills";
@@ -6,9 +7,11 @@ import Certifications from "./components/Certifications";
 import Contact from "./components/Contact"; 
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <>
+    <Navbar />
       <Header />
       <About />
       <Skills />
---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+const Navbar = () => {
+  return (
+    <nav>
+      {/* Portfolio / name */}
+      <h1>Thembela</h1>
+
+      {/* Navigation links */}
+      <ul>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+         <li>
+          <a href="#certifications">Certifications</a>
+        </li>
+         <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
